94be1e4c9d637cdb3472e0e01769839de4f63687
Mark Hakansson <44841869+markhakansson@users.noreply.github.com>
2026-06-15 14:21:28 +0200

    Package kirin with nix (#3)
    Co-authored-by: Mark Hakansson <mark.hakansson@kiteshield.com>

--- flake.nix
+++ flake.nix
@@ -7,14 +7,26 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        kirin = pkgs.rustPlatform.buildRustPackage {
+          pname = "kirin";
+          version = "0.1.0";
+          src = self;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
       in
       {
+        packages = {
+          inherit kirin;
+          default = kirin;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             rustToolchain
